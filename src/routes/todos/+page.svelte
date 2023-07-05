53be281459038619d19a0330f8999bb8cb9f3f20
Amar Gill <amardeep@clearbanc.com>
2023-07-04 23:04:56 -0400

    add todos page

---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,24 @@
+<h1>Todos</h1>
+<form>
+	<fieldset>
+		<legend>Add a Todo item</legend>
+		<label for="title">Title</label>
+		<input type="text" name="title" />
+		<label for="content">Content</label>
+		<input type="text" name="content" />
+		<div>
+			<button type="submit">Add</button>
+			<button type="submit">Delete</button>
+		</div>
+	</fieldset>
+</form>
+
+<style>
+	form > fieldset {
+		display: flex;
+		flex-direction: column;
+	}
+	form > fieldset button {
+		text-transform: uppercase;
+	}
+</style>
