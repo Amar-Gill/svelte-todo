53051cb866950b7492177f69703bc8070b196870
Amar Gill <amardeep@clearbanc.com>
2023-07-06 23:50:27 -0400

    complete form action

--- src/routes/todos/+page.server.ts
+++ src/routes/todos/+page.server.ts
@@ -1,7 +1,13 @@
+import { db } from '$lib/server';
+import { todos } from '$lib/server/schema';
 import type { Actions } from './$types';
 
 export const actions = {
 	add: async ({ request }) => {
-		console.log(await request.formData());
+		const formData = await request.formData();
+		await db.insert(todos).values({
+			title: formData.get('string')?.toString(),
+			content: formData.get('content')?.toString()
+		});
 	}
 } satisfies Actions;
