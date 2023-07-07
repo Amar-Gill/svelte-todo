7ff163d4995afbc895a6a2d94a8284246d69b4bc
Amar Gill <amardeep@clearbanc.com>
2023-07-06 22:14:05 -0400

    set db url correctly in db client

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,11 +1,10 @@
+import { dburl } from '$env/static/private';
 import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const dbUrl = import.meta.env.dburl;
-
 // for migrations
-export const migrationClient = postgres(dbUrl, { max: 1 });
+export const migrationClient = postgres(dburl, { max: 1 });
 
 // for query purposes
-const queryClient = postgres(dbUrl);
+const queryClient = postgres(dburl);
 export const db: PostgresJsDatabase = drizzle(queryClient);
