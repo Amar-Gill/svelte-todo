2a1a3313aa25bbaa4eb5f6d1306b636b5f4c29b2
Amar Gill <amardeep@clearbanc.com>
2023-07-14 17:23:02 -0400

    add completed column to todos table

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,7 +1,8 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const todos = pgTable('todos', {
 	id: serial('id').primaryKey(),
 	title: text('title'),
-	content: text('content')
+	content: text('content'),
+	completed: boolean('completed').default(false)
 });
