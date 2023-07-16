f9a6d6a44f60ff12ebfadeda38e3e75903a054a9
Amar Gill <amardeep@clearbanc.com>
2023-07-16 13:20:06 -0400

    move todo actions in lib

--- src/lib/server/drizzle/index.ts
+++ src/lib/server/drizzle/index.ts
@@ -1,6 +1,9 @@
 import { dburl } from '$env/static/private';
+import type { Action } from '@sveltejs/kit';
 import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { todos } from './schema';
+import { eq } from 'drizzle-orm';
 
 // for migrations
 export const migrationClient = postgres(dburl, { max: 1 });
@@ -8,3 +11,23 @@ export const migrationClient = postgres(dburl, { max: 1 });
 // for query purposes
 const queryClient = postgres(dburl);
 export const db: PostgresJsDatabase = drizzle(queryClient);
+
+// todos
+export const addTodo: Action = async ({ request }) => {
+	const formData = await request.formData();
+	await db.insert(todos).values({
+		title: formData.get('title')?.toString(),
+		content: formData.get('content')?.toString()
+	});
+};
+
+export const toggleTodoComplete: Action = async ({ request, url }) => {
+	const formData = await request.formData();
+	const completed = parseBool(formData.get('completed')?.toString());
+	const id = parseInt(url.searchParams.get('id'));
+	await db.update(todos).set({ completed }).where(eq(todos.id, id));
+};
+
+function parseBool(s?: string): boolean {
+	return s === 'true' ? true : false;
+}
