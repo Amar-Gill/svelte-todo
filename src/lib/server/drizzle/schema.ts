7bb9ece040db115756e0862ab6031f01ae859434
Amar Gill <amardeep@clearbanc.com>
2023-07-30 16:01:36 -0400

    add user_id to todo schema

--- src/lib/server/drizzle/schema.ts
+++ src/lib/server/drizzle/schema.ts
@@ -4,5 +4,6 @@ export const todos = pgTable('todos', {
 	id: serial('id').primaryKey(),
 	title: text('title'),
 	content: text('content'),
-	completed: boolean('completed').default(false)
+	completed: boolean('completed').default(false),
+	userId: text('user_id')
 });
